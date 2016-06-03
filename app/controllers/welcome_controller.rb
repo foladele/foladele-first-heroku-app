150b95b95998841dbe21cfe4103469f928be4429
Faith Oladele <faitholadele@gmail.com>
2016-06-03 12:16:07 -0600

    add pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-  	@animals = Animal.all
+  	# @animals = Animal.all
+  	@animals= Animal.paginate(page: params[:page], per_page: 10)
+
   end
 end
